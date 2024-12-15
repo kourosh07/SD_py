afea1f5dcd35beb77db778c3326526d4a14f5a96
Kourosh Rahimi <kourosh2004rahimi@gmail.com>
2024-12-15 14:23:13 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,102 +1,51 @@
-# O(1)
+# کلاس استک
 
-def worker(arr, index):
-    return arr[index]
-
-arr = [1, 2, 3, 4, 5]
-index = 2
-print(worker(arr, index))  
-
-# O(n)
-
-def sum(arr):
-    total = 0
-    for element in arr:
-        total += element
-    return total
-
-arr = [1, 2, 3, 4, 5]
-print(sum(arr)) 
-
-# O(log(n))
-
-def binary_search(arr, target):
-    low = 0
-    high = len(arr) - 1
-    
-    while low <= high:
-        mid = (low + high) // 2
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            low = mid + 1
-        else:
-            high = mid - 1
-            
-    return -1  
-
-arr = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
-target = 8
-result = binary_search(arr, target)
-if result != -1:
-    print(f"عنصر {target} در شاخص {result} پیدا شد.")
-else:
-    print(f"عنصر {target} پیدا نشد.")
-
-# O(nlog(n))
-
-def merge_sort(arr):
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        
-        left_half = arr[:mid]
-        right_half = arr[mid:]
-        
-        merge_sort(left_half)
-        merge_sort(right_half)
-        
-        i = j = k = 0
-        
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                arr[k] = left_half[i]
-                i += 1
-            else:
-                arr[k] = right_half[j]
-                j += 1
-            k += 1
-        
-        while i < len(left_half):
-            arr[k] = left_half[i]
-            i += 1
-            k += 1
-        
-        while j < len(right_half):
-            arr[k] = right_half[j]
-            j += 1
-            k += 1
-
-arr = [38, 27, 43, 3, 9, 82, 10]
-print("آرایه قبل از مرتب‌سازی:", arr)
-merge_sort(arr)
-print("آرایه بعد از مرتب‌سازی:", arr)
-
-################################################################
 class Stack:
-    def __init__(self, limit=100):
-        self.Stack=[]
-        self.limit= limit
-    
-    def push(self, data):
-        if len(self.Stack)>=self.limit:
+    def __init__(self, limit=10):
+        self.stack = []
+        self.limit = limit
+
+    def peek(self):
+        if len(self.stack) != 0:
             return -1
         else:
-            self.Stack.append(data)
+            return self.stack[-1]
+
+    def push(self, data):
+        if len(self.stack) > self.limit:
+            return -1
+        else:
+            self.stack.append(data)
 
     def pop(self):
-        if len(self.Stack)<=self.limit:
+
+        if len(self.stack) >= 0:
+            return self.stack.pop()
+        else:
+            return -1
+
+    def find(self, data):
+        if len(self.stack) <= 0:
+            return -1
+        for i in range(1, len(self.stack)):
+            if self.stack[i] == data:
+                return i
+        return -1
+    def replace(self, data, new_data):
+        if len(self.stack) <= 0:
             return -1
         else:
-            self.Stack.pop()
+            if data in self.stack:
+                self.stack[self.stack.index(data)] = new_data
+            else:
+                return -1
 
-##################################################################
+    def show(self):
+        for i in self.stack:
+            print(i)
+
+    def is_empty(self):
+        return len(self.stack) == 0
+    
+    def __len__(self):
+        return len(self.stack)
