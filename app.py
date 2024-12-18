8ccde00e5f3430a24321077fe53dad434aea80ee
Kourosh Rahimi <kourosh2004rahimi@gmail.com>
2024-12-18 11:54:01 +0330

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,58 @@ def sum(arr):
     
 ################################################################################################################################################
 
+# O(log(n))
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+            
+    return -1  
 
 ################################################################################################################################################
 
+# O(nlog(n))
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+        
+        merge_sort(left_half)
+        merge_sort(right_half)
+        
+        i = j = k = 0
+        
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+        
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+        
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+    
 ################################################################################################################################################
 
 # کلاس استک
